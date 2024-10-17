9f4c15dc01d35767829ee923d1aa7a4d4cc24c40
Re'em Kishinevsky <Re'em Kishinevsky>
2024-10-17 14:02:20 +0300

    colorscheme tweaks

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,7 +1,8 @@
 return {
-  "sainnhe/sonokai",
+  "navarasu/onedark.nvim",
   lazy = false,     -- make sure we load this during startup
   priority = 1000,  -- make sure to load this before all the other start plugins
-  init = function() vim.cmd.colorscheme "sonokai" end,
-  enabled = true
+  init = function() require('onedark').load() end,
+  opts = { style = 'dark', transparent = true },
+  enabled = false
 }
