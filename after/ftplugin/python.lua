a4ef57f981f8f44b3d5c601dc9df7831f69d8ded
Re'em Kishinevsky <Re'em Kishinevsky>
2024-10-17 14:01:10 +0300

    remove unsued ftplugins; tweak python ftplugin

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,5 +1,6 @@
 -- Show line after desired maximum text width
-vim.cmd('setlocal colorcolumn=89')
+vim.cmd([[setlocal textwidth=89]])
+vim.cmd([[setlocal colorcolumn=+1]])
 
 -- Indentation
 vim.g.pyindent_open_paren = 'shiftwidth()'
